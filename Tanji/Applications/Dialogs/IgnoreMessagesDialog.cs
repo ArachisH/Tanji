bc9822473367aa2f2f1c3658a4760b73bcf26cfe
ArachisH <iKBenPeanut@gmail.com>
2017-10-23 19:14:54 -0700

    There was a bug somewhere in here, but we fixed it so it's ok now, go home

--- Tanji/Applications/Dialogs/IgnoreMessagesDialog.cs
+++ Tanji/Applications/Dialogs/IgnoreMessagesDialog.cs
@@ -1,111 +1,90 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.ComponentModel;
 using System.Collections.Generic;
 
 using Tanji.Components;
 
 namespace Tanji.Applications.Dialogs
 {
-    public partial class IgnoreMessagesDialog : TanjiForm
+    [DesignerCategory("Form")]
+    public partial class IgnoreMessagesDialog : ObservableForm
     {
-        private readonly IDictionary<ushort, bool> _ignoredOutgoing, _ignoredIncoming;
+        private readonly Dictionary<int, bool> _ignoredMessages;
 
-        private ushort _header = 0;
-        public ushort Header
+        private ushort _id = 0;
+        public ushort Id
         {
-            get { return _header; }
+            get => _id;
             set
             {
-                _header = value;
-                RaiseOnPropertyChanged(nameof(Header));
+                _id = value;
+                RaiseOnPropertyChanged();
             }
         }
 
-        public IgnoreMessagesDialog(
-            IDictionary<ushort, bool> ignoredOutgoing,
-            IDictionary<ushort, bool> ignoredIncoming)
+        public IgnoreMessagesDialog(Dictionary<int, bool> ignoredMessages)
         {
-            _ignoredOutgoing = ignoredOutgoing;
-            _ignoredIncoming = ignoredIncoming;
+            _ignoredMessages = ignoredMessages;
 
             InitializeComponent();
-            DisplayIgnoredMessages("Outgoing", _ignoredOutgoing);
-            DisplayIgnoredMessages("Incoming", _ignoredIncoming);
 
-            HeaderTxt.DataBindings.Add("Value", this,
-                nameof(Header), false, DataSourceUpdateMode.OnPropertyChanged);
+            IgnoredVw.BeginUpdate();
+            foreach (int container in ignoredMessages.Keys)
+            {
+                var id = (ushort)((container >> 1) & ushort.MaxValue);
+                bool isOutgoing = ((container & 1) == 1);
+
+                string type = (isOutgoing ? "Outgoing" : "Incoming");
+                ListViewItem item = IgnoredVw.AddItem(type, id);
+
+                item.Checked = ignoredMessages[id];
+            }
+            IgnoredVw.EndUpdate();
+
+            Bind(HeaderTxt, "Value", nameof(Id));
 
             TypeTxt.SelectedIndex = 0;
-
             IgnoredVw.ItemChecked += IgnoredVw_ItemChecked;
             IgnoredVw.ItemSelectionStateChanged += IgnoredVw_ItemSelectionStateChanged;
         }
 
         private void RemoveBtn_Click(object sender, EventArgs e)
         {
-            ListViewItem item = IgnoredVw.SelectedItem;
-            var ignoredMsg = (item.Tag as Tuple<ushort, bool>);
-            if (ignoredMsg != null)
+            var container = (ushort)IgnoredVw.SelectedItem.Tag;
+            if (_ignoredMessages.ContainsKey(container))
             {
-                ushort header = ignoredMsg.Item1;
-                bool isOutgoing = ignoredMsg.Item2;
-
-                IDictionary<ushort, bool> ignored = (isOutgoing ?
-                    _ignoredOutgoing : _ignoredIncoming);
-
-                if (ignored.ContainsKey(header))
-                    ignored.Remove(header);
+                _ignoredMessages.Remove(container);
             }
             IgnoredVw.RemoveSelectedItem();
         }
         private void AddHeaderBtn_Click(object sender, EventArgs e)
         {
-            IgnoreMessage(Header, (TypeTxt.Text == "Outgoing"));
+            ushort container = 0;
+            bool isOutgoing = (TypeTxt.Text == "Outgoing");
+            container |= (ushort)((Id & ushort.MaxValue) << 1);
+            container |= (ushort)(isOutgoing ? 1 : 0);
+
+            if (!_ignoredMessages.ContainsKey(container))
+            {
+                _ignoredMessages[container] = true;
+
+                var item = new ListViewItem(new string[] { TypeTxt.Text, Id.ToString() });
+                item.Tag = container;
+                item.Checked = true;
+
+                IgnoredVw.AddItem(item);
+            }
         }
 
         private void IgnoredVw_ItemChecked(object sender, ItemCheckedEventArgs e)
         {
-            var ignoredMsg = (e.Item.Tag as Tuple<ushort, bool>);
-            if (ignoredMsg != null)
-            {
-                ushort header = ignoredMsg.Item1;
-                bool isOutgoing = ignoredMsg.Item2;
-
-                IDictionary<ushort, bool> ignored = (isOutgoing ?
-                    _ignoredOutgoing : _ignoredIncoming);
-
-                ignored[header] = e.Item.Checked;
-            }
+            var container = (ushort)e.Item.Tag;
+            _ignoredMessages[container] = e.Item.Checked;
         }
         private void IgnoredVw_ItemSelectionStateChanged(object sender, EventArgs e)
         {
             RemoveBtn.Enabled = IgnoredVw.HasSelectedItem;
-        }
-
-        private void IgnoreMessage(ushort header, bool isOutgoing)
-        {
-            IDictionary<ushort, bool> ignored = (isOutgoing ?
-                _ignoredOutgoing : _ignoredIncoming);
-
-            if (!ignored.ContainsKey(header))
-            {
-                ignored[header] = true;
-
-                ListViewItem item = IgnoredVw.AddItem(
-                    (isOutgoing ? "Outgoing" : "Incoming"), header);
-
-                item.Checked = true;
-                item.Tag = new Tuple<ushort, bool>(header, isOutgoing);
-            }
-        }
-        private void DisplayIgnoredMessages(string type, IDictionary<ushort, bool> ignored)
-        {
-            foreach (ushort header in ignored.Keys)
-            {
-                ListViewItem item = IgnoredVw.AddItem(type, header);
-                item.Tag = new Tuple<ushort, bool>(header, (type == "Outgoing"));
-                item.Checked = ignored[header];
-            }
         }
 
         protected override void OnLoad(EventArgs e)
