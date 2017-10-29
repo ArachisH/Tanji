44db8a746c08cd728820bc2c462d49fe7f307eef
ArachisH <HuitronAJ@gmail.com>
2017-10-28 19:16:41 -0700

    Throw a HashResolvingException when no match is found for the specified message hash

--- Sulakore/Habbo/Messages/HashResolvingException.cs
+++ Sulakore/Habbo/Messages/HashResolvingException.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Sulakore.Habbo.Messages
 {
     public sealed class HashResolvingException : Exception
     {
         public string Revision { get; }
-        public Dictionary<string, string[]> Unresolved { get; }
+        public ReadOnlyDictionary<string, string[]> Unresolved { get; }
 
+        public HashResolvingException(string revision, IList<string> hashes)
+            : base($"Failed to resolve '{hashes.Count}' hash value(s) from revision '{revision}'.")
+        {
+            Unresolved = new ReadOnlyDictionary<string, string[]>(
+                hashes.ToDictionary(u => u, u => new string[0]));
+        }
         public HashResolvingException(string revision, IDictionary<string, IList<string>> unresolved)
             : base($"Failed to resolve '{unresolved.Count}' hash value(s) from revision '{revision}'.")
         {
-            Unresolved = unresolved
-                .ToDictionary(u => u.Key, u => u.Value.ToArray());
+            Unresolved = new ReadOnlyDictionary<string, string[]>(
+                unresolved.ToDictionary(u => u.Key, u => u.Value.ToArray()));
         }
     }
 }
