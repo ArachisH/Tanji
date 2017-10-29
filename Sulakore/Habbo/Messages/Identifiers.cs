ae4adba2ebc63dd00a47fb83636b3899f6cb6b14
ArachisH <iKBenPeanut@gmail.com>
2017-10-29 12:58:43 -0700

    Added method to return a name given a packet id

--- Sulakore/Habbo/Messages/Identifiers.cs
+++ Sulakore/Habbo/Messages/Identifiers.cs
@@ -6,26 +6,38 @@ namespace Sulakore.Habbo.Messages
     public abstract class Identifiers
     {
         private readonly string _section;
-        private readonly Dictionary<string, ushort> _ids;
+        private readonly Dictionary<ushort, string> _namesById;
+        private readonly Dictionary<string, ushort> _idsByName;
         private readonly Dictionary<string, string> _namesByHash;
 
         public Identifiers()
         {
             _section = GetType().Name;
-            _ids = new Dictionary<string, ushort>();
+            _namesById = new Dictionary<ushort, string>();
+            _idsByName = new Dictionary<string, ushort>();
             _namesByHash = new Dictionary<string, string>();
         }
 
         public ushort this[string name]
         {
-            get => _ids[name];
-            set => _ids[name] = value;
+            get => _idsByName[name];
+            set => _idsByName[name] = value;
         }
 
+        public string GetName(ushort id)
+        {
+            _namesById.TryGetValue(id, out string name);
+            return name;
+        }
         public string GetName(string hash)
         {
             _namesByHash.TryGetValue(hash, out string name);
             return name;
+        }
+
+        public bool TryGetId(string name, out ushort id)
+        {
+            return _idsByName.TryGetValue(name, out id);
         }
 
         public void Save(string path)
@@ -38,14 +50,14 @@ namespace Sulakore.Habbo.Messages
         public void Save(StreamWriter output)
         {
             output.WriteLine($"[{_section}]");
-            foreach (string name in _ids.Keys)
+            foreach (string name in _idsByName.Keys)
             {
-                output.WriteLine($"{name}={_ids[name]}");
+                output.WriteLine($"{name}={_idsByName[name]}");
             }
         }
         public void Load(HGame game, string path)
         {
-            _ids.Clear();
+            _idsByName.Clear();
             _namesByHash.Clear();
             using (var input = new StreamReader(path))
             {
@@ -73,7 +85,11 @@ namespace Sulakore.Habbo.Messages
                             }
                         }
 
-                        _ids[name] = id;
+                        if (id != ushort.MaxValue)
+                        {
+                            _namesById[id] = name;
+                        }
+                        _idsByName[name] = id;
                         GetType().GetProperty(name)?.SetValue(this, id);
                     }
                 }
