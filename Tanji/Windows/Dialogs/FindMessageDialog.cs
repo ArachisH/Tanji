b5d36b6c2c6fc9b67b5cdafdb8e3ecc084cd19fb
ArachisH <HuitronAJ@gmail.com>
2017-11-18 20:03:43 -0800

    Forgot if I was done with this, idk

--- Tanji/Windows/Dialogs/FindMessageDialog.cs
+++ Tanji/Windows/Dialogs/FindMessageDialog.cs
@@ -6,13 +6,14 @@ using System.Collections.Generic;
 using Tanji.Components;
 
 using Sulakore.Habbo;
+using Sulakore.Habbo.Messages;
 
 namespace Tanji.Windows.Dialogs
 {
     [DesignerCategory("Form")]
     public partial class FindMessageDialog : ObservableForm
     {
-        private readonly HGame _game;
+        private readonly MainFrm _mainUI;
 
         private string _hash = string.Empty;
         public string Hash
@@ -25,39 +26,97 @@ namespace Tanji.Windows.Dialogs
             }
         }
 
-        public FindMessageDialog(HGame game, string hash)
+        private ushort _id = 0;
+        public ushort Id
         {
-            _game = game;
+            get => _id;
+            set
+            {
+                _id = value;
+                RaiseOnPropertyChanged();
+            }
+        }
+
+        public FindMessageDialog(MainFrm mainUI, string selectedText)
+        {
+            _mainUI = mainUI;
 
             InitializeComponent();
+            PropertyChanged += FindMessageDialog_PropertyChanged;
 
+            Bind(IdNum, "Value", nameof(Id));
             Bind(HashTxt, "Text", nameof(Hash));
-
-            Hash = hash;
-            HashTxt.SelectAll();
         }
 
         private void FindBtn_Click(object sender, EventArgs e)
         {
             HeadersVw.ClearItems();
-
-            List<MessageItem> messages = null;
-            if (!_game.Messages.TryGetValue(Hash, out messages))
+            if (!TryDisplayMessages(Hash))
             {
-                MessageBox.Show("Unable to find any messages that are associated with the specified hash.",
-                    "Tanji - Alert!", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
-
                 HashTxt.Select();
                 HashTxt.SelectAll();
                 return;
             }
+        }
+        private void HeadersVw_ItemSelected(object sender, EventArgs e)
+        {
+            Console.WriteLine(nameof(HeadersVw_ItemSelected));
+        }
+        private void FindMessageDialog_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(Id):
+                {
+                    TryDisplayMessages(Id);
+                    IdNum.Focus();
+                    break;
+                }
+            }
+        }
 
+        private bool TryDisplayMessages(ushort id)
+        {
+            HeadersVw.Items.Clear();
+            if (_mainUI.Game.InMessages.TryGetValue(id, out MessageItem inMessage))
+            {
+                DisplayMessage(inMessage);
+            }
+            if (_mainUI.Game.OutMessages.TryGetValue(id, out MessageItem outMessage))
+            {
+                DisplayMessage(outMessage);
+            }
+            return (inMessage != null || outMessage != null);
+        }
+        private bool TryDisplayMessages(string hash)
+        {
+            HeadersVw.Items.Clear();
+            if (!_mainUI.Game.Messages.TryGetValue(hash, out List<MessageItem> messages)) return false;
+
+            ListViewItem lastItem = null;
             foreach (MessageItem message in messages)
             {
-                string type = (message.IsOutgoing ? "Outgoing" : "Incoming");
-                ListViewItem item = HeadersVw.AddFocusedItem(type, message.Id, message.Class.QName.Name);
-                item.Tag = message;
+                lastItem = DisplayMessage(message);
             }
+            return true;
+        }
+        private ListViewItem DisplayMessage(MessageItem message)
+        {
+            string type = (message.IsOutgoing ? "Outgoing" : "Incoming");
+            var identifiers = (message.IsOutgoing ? _mainUI.Out : (Identifiers)_mainUI.In);
+            ListViewItem item = HeadersVw.AddItem(type, message.Id, "<Unknown>", message.Class.QName.Name);
+
+            string name = identifiers.GetName(message.Hash);
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                item.SubItems[2].Text = name;
+            }
+
+            if (!message.IsOutgoing)
+            {
+                item.SubItems.Add(message.Parser.QName.Name);
+            }
+            return item;
         }
 
         protected override void OnLoad(EventArgs e)
--- Tanji/Windows/Dialogs/FindMessageDialog.designer.cs
+++ Tanji/Windows/Dialogs/FindMessageDialog.designer.cs
@@ -33,15 +33,20 @@
             this.FindBtn = new Sulakore.Components.SKoreButton();
             this.HeadersVw = new Sulakore.Components.SKoreListView();
             this.TypeCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.HeaderCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.ClassParserNameCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.NameCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ClassNameCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ParserNameCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Glow1Pnl = new System.Windows.Forms.Panel();
+            this.IdNum = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.IDCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.IdNum)).BeginInit();
             this.SuspendLayout();
             // 
             // HashLbl
             // 
             this.HashLbl.AutoSize = true;
-            this.HashLbl.Location = new System.Drawing.Point(12, 17);
+            this.HashLbl.Location = new System.Drawing.Point(100, 15);
             this.HashLbl.Name = "HashLbl";
             this.HashLbl.Size = new System.Drawing.Size(35, 13);
             this.HashLbl.TabIndex = 0;
@@ -49,18 +54,18 @@
             // 
             // HashTxt
             // 
-            this.HashTxt.Location = new System.Drawing.Point(47, 14);
+            this.HashTxt.Location = new System.Drawing.Point(135, 12);
             this.HashTxt.Name = "HashTxt";
-            this.HashTxt.Size = new System.Drawing.Size(215, 20);
+            this.HashTxt.Size = new System.Drawing.Size(243, 20);
             this.HashTxt.TabIndex = 1;
             this.HashTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // FindBtn
             // 
             this.FindBtn.BackColor = System.Drawing.Color.Transparent;
-            this.FindBtn.Location = new System.Drawing.Point(268, 12);
+            this.FindBtn.Location = new System.Drawing.Point(384, 12);
             this.FindBtn.Name = "FindBtn";
-            this.FindBtn.Size = new System.Drawing.Size(66, 22);
+            this.FindBtn.Size = new System.Drawing.Size(66, 20);
             this.FindBtn.Skin = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(63)))), ((int)(((byte)(63)))));
             this.FindBtn.TabIndex = 2;
             this.FindBtn.Text = "Find";
@@ -71,61 +76,99 @@
             this.HeadersVw.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.HeadersVw.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.TypeCol,
-            this.HeaderCol,
-            this.ClassParserNameCol});
+            this.IDCol,
+            this.NameCol,
+            this.ClassNameCol,
+            this.ParserNameCol});
             this.HeadersVw.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.HeadersVw.FullRowSelect = true;
             this.HeadersVw.GridLines = true;
             this.HeadersVw.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.HeadersVw.HideSelection = false;
-            this.HeadersVw.Location = new System.Drawing.Point(0, 41);
+            this.HeadersVw.Location = new System.Drawing.Point(0, 39);
             this.HeadersVw.MultiSelect = false;
             this.HeadersVw.Name = "HeadersVw";
             this.HeadersVw.ShowItemToolTips = true;
-            this.HeadersVw.Size = new System.Drawing.Size(346, 126);
+            this.HeadersVw.Size = new System.Drawing.Size(462, 126);
             this.HeadersVw.TabIndex = 3;
             this.HeadersVw.UseCompatibleStateImageBehavior = false;
             this.HeadersVw.View = System.Windows.Forms.View.Details;
+            this.HeadersVw.ItemSelected += new System.EventHandler(this.HeadersVw_ItemSelected);
             // 
             // TypeCol
             // 
             this.TypeCol.Text = "Type";
             // 
-            // HeaderCol
+            // NameCol
             // 
-            this.HeaderCol.Text = "Header";
+            this.NameCol.Text = "Name";
+            this.NameCol.Width = 125;
             // 
-            // ClassParserNameCol
+            // ClassNameCol
             // 
-            this.ClassParserNameCol.Text = "Class/Parser Name";
-            this.ClassParserNameCol.Width = 205;
+            this.ClassNameCol.Text = "Class Name";
+            this.ClassNameCol.Width = 116;
+            // 
+            // ParserNameCol
+            // 
+            this.ParserNameCol.Text = "Parser Name";
+            this.ParserNameCol.Width = 116;
             // 
             // Glow1Pnl
             // 
             this.Glow1Pnl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(63)))), ((int)(((byte)(63)))));
             this.Glow1Pnl.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.Glow1Pnl.Location = new System.Drawing.Point(0, 40);
+            this.Glow1Pnl.Location = new System.Drawing.Point(0, 38);
             this.Glow1Pnl.Name = "Glow1Pnl";
-            this.Glow1Pnl.Size = new System.Drawing.Size(346, 1);
+            this.Glow1Pnl.Size = new System.Drawing.Size(462, 1);
             this.Glow1Pnl.TabIndex = 4;
+            // 
+            // IdNum
+            // 
+            this.IdNum.Location = new System.Drawing.Point(33, 12);
+            this.IdNum.Maximum = new decimal(new int[] {
+            4000,
+            0,
+            0,
+            0});
+            this.IdNum.Name = "IdNum";
+            this.IdNum.Size = new System.Drawing.Size(61, 20);
+            this.IdNum.TabIndex = 5;
+            this.IdNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ID:";
+            // 
+            // IDCol
+            // 
+            this.IDCol.Text = "ID";
+            this.IDCol.Width = 45;
             // 
             // FindMessageDialog
             // 
             this.AcceptButton = this.FindBtn;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(346, 167);
+            this.ClientSize = new System.Drawing.Size(462, 165);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.IdNum);
             this.Controls.Add(this.Glow1Pnl);
             this.Controls.Add(this.HashLbl);
             this.Controls.Add(this.HeadersVw);
             this.Controls.Add(this.FindBtn);
             this.Controls.Add(this.HashTxt);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FindMessageDialog";
             this.ShowInTaskbar = false;
             this.Text = "Tanji ~ Find Message";
+            ((System.ComponentModel.ISupportInitialize)(this.IdNum)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -137,9 +180,13 @@
         private Sulakore.Components.SKoreButton FindBtn;
         private Sulakore.Components.SKoreListView HeadersVw;
         private System.Windows.Forms.ColumnHeader TypeCol;
-        private System.Windows.Forms.ColumnHeader HeaderCol;
-        private System.Windows.Forms.ColumnHeader ClassParserNameCol;
+        private System.Windows.Forms.ColumnHeader ClassNameCol;
         internal System.Windows.Forms.TextBox HashTxt;
         private System.Windows.Forms.Panel Glow1Pnl;
+        private System.Windows.Forms.NumericUpDown IdNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColumnHeader NameCol;
+        private System.Windows.Forms.ColumnHeader ParserNameCol;
+        private System.Windows.Forms.ColumnHeader IDCol;
     }
 }
