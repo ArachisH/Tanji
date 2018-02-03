25688faf1995c5597bc25196492f2221721a68fd
ArachisH <iKBenPeanut@gmail.com>
2018-02-03 14:16:26 -0800

    Removed HConnection Added SendToXAsync method that takes an HMessage type parameter

--- Sulakore/Communication/IHConnection.cs
+++ Sulakore/Communication/IHConnection.cs
@@ -1,20 +1,33 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+
+using Sulakore.Protocol;
 
 namespace Sulakore.Communication
 {
     public interface IHConnection
     {
+        [Obsolete]
         ushort Port { get; }
+        [Obsolete]
         string Host { get; }
+        [Obsolete]
         string Address { get; }
 
+        [Obsolete]
         int TotalOutgoing { get; }
+        [Obsolete]
         int TotalIncoming { get; }
 
+        HNode Local { get; }
+        HNode Remote { get; }
+
         Task<int> SendToServerAsync(byte[] data);
-        Task<int> SendToServerAsync(ushort header, params object[] values);
+        Task<int> SendToServerAsync(HMessage packet);
+        Task<int> SendToServerAsync(ushort id, params object[] values);
 
         Task<int> SendToClientAsync(byte[] data);
-        Task<int> SendToClientAsync(ushort header, params object[] values);
+        Task<int> SendToClientAsync(HMessage packet);
+        Task<int> SendToClientAsync(ushort id, params object[] values);
     }
 }
