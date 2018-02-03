39930819e6e17724e904778e73c5a7ebf388fbb2
ArachisH <iKBenPeanut@gmail.com>
2018-02-02 19:05:41 -0800

    Cleaned up some stuff, and left a TODO for me, thanks me

--- Tanji/Pages/About/AboutPage.cs
+++ Tanji/Pages/About/AboutPage.cs
@@ -5,38 +5,30 @@ using System.Windows.Forms;
 using System.Threading.Tasks;
 
 using Tanji.Windows;
-using Tangine.GitHub;
 
 namespace Tanji.Pages.About
 {
     public class AboutPage : TanjiPage
     {
-        public GitRepository TanjiRepo { get; }
-
         public Version LocalVersion { get; }
         public Version LatestVersion { get; private set; }
 
         public AboutPage(MainFrm ui, TabPage tab)
             : base(ui, tab)
         {
-            TanjiRepo = new GitRepository("ArachisH", "Tanji");
             LocalVersion = Assembly.GetExecutingAssembly().GetName().Version;
 
             UI.Shown += UI_Shown;
-            UI.TanjiVersionTxt.Text = ("v" + LocalVersion);
+            UI.TanjiVersionTxt.Text = ("v" + LocalVersion.ToString(3));
 
             UI.ArachisBtn.Click += ArachisBtn_Click;
-            UI.DarkStarBtn.Click += DarkStarBtn_Click;
             UI.SpeaqerBtn.Click += SpeaqerBtn_Click;
+            UI.DarkStarBtn.Click += DarkStarBtn_Click;
 
             UI.DonateBtn.Click += DonateBtn_Click;
 
-            UI.HarbleBtn.Click += HarbleBtn_Click;
-            UI.H4BB0Btn.Click += H4BB0Btn_Click;
-
-            UI.DarkboxBtn.Click += DarkboxBtn_Click;
             UI.SNGButton.Click += SNGButton_Click;
-            UI.ForbiddenBtn.Click += ForbiddenBtn_Click;
+            UI.DarkboxBtn.Click += DarkboxBtn_Click;
         }
 
         private void ArachisBtn_Click(object sender, EventArgs e)
@@ -54,16 +46,8 @@ namespace Tanji.Pages.About
 
         private void DonateBtn_Click(object sender, EventArgs e)
         {
+            Process.Start("https://beerpay.io/ArachisH/Tanji");
             Process.Start("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=HMYZ4GB5N2PAU");
-        }
-
-        private void HarbleBtn_Click(object sender, EventArgs e)
-        {
-            Process.Start("https://discord.gg/Vyc2gFC");
-        }
-        private void H4BB0Btn_Click(object sender, EventArgs e)
-        {
-            Process.Start("https://discord.gg/v2e6GdJ");
         }
 
         private void DarkboxBtn_Click(object sender, EventArgs e)
@@ -74,32 +58,11 @@ namespace Tanji.Pages.About
         {
             Process.Start("https://sngforum.info/");
         }
-        private void ForbiddenBtn_Click(object sender, EventArgs e)
-        {
-            Process.Start("https://forbidden.sh");
-        }
 
-        private async void UI_Shown(object sender, EventArgs e)
+        private void UI_Shown(object sender, EventArgs e)
         {
             UI.Shown -= UI_Shown;
-            await Task.Delay(225);
-
-            GitRelease latestRelease =
-                await TanjiRepo.GetLatestReleaseAsync();
-
-            if (latestRelease != null)
-            {
-                LatestVersion = new Version(
-                    latestRelease.TagName.Substring(1));
-
-                UI.TanjiVersionTxt.IsLink = true;
-
-                if (LatestVersion > LocalVersion &&
-                   !latestRelease.IsPrerelease)
-                {
-                    UI.TanjiVersionTxt.Text = "Update Found!";
-                }
-            }
+            // TODO: Compare this local build, against latest one in GitHub using Octokit
         }
     }
 }
