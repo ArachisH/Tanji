95b6313bd10ef0e57bd58b13ee015436f363adf0
ArachisH <iKBenPeanut@gmail.com>
2018-02-02 18:25:38 -0800

    Remove this...

--- Tanji/Properties/Resources.Designer.cs
+++ Tanji/Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace Tanji.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap _4282632170 {
-            get {
-                object obj = ResourceManager.GetObject("_4282632170", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap Avatar {
             get {
                 object obj = ResourceManager.GetObject("Avatar", resourceCulture);
