2ebf8ca23966c31858d5830bf8390873c7dc61db
ArachisH <iKBenPeanut@gmail.com>
2018-02-03 14:21:12 -0800

    We need this

--- Tanji/Windows/MainFrm.cs
+++ Tanji/Windows/MainFrm.cs
@@ -6,6 +6,7 @@ using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 
+using Tanji.Network;
 using Tanji.Utilities;
 using Tanji.Properties;
 using Tanji.Components;
