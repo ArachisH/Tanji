72f29bf35b07d29c09cb2a914002a55c11dd774a
ArachisH <iKBenPeanut@gmail.com>
2018-02-04 20:54:53 -0800

    Absolutely, do not, ever allow the user to run this application under administrative privileges(Will re-open itself without elevation)

--- Tanji/Program.cs
+++ Tanji/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
+using System.Security.Principal;
 
 using Tanji.Windows;
 
 using Eavesdrop;
+
+using static Tanji.Utilities.NativeMethods;
 
 namespace Tanji
 {
@@ -15,6 +20,14 @@ namespace Tanji
             AppDomain.CurrentDomain.UnhandledException += UnhandledException;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            bool isElevated = new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
+            if (isElevated)
+            {
+                MessageBox.Show("STOP RUNNING AS ADMINISTRATOR FOR THE LOVE OF GOD");
+                RunAsDesktopUser(Assembly.GetEntryAssembly().Location);
+                return;
+            }
 
             Eavesdropper.Certifier = new CertificateManager("Tanji", "Tanji Certificate Authority");
             Eavesdropper.Overrides.AddRange(new[]
@@ -38,7 +51,97 @@ namespace Tanji
 
             Application.Run(new MainFrm());
         }
+        private static void RunAsDesktopUser(string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(fileName));
+            }
 
+            // To start process as shell user you will need to carry out these steps:
+            // 1. Enable the SeIncreaseQuotaPrivilege in your current token
+            // 2. Get an HWND representing the desktop shell (GetShellWindow)
+            // 3. Get the Process ID(PID) of the process associated with that window(GetWindowThreadProcessId)
+            // 4. Open that process(OpenProcess)
+            // 5. Get the access token from that process (OpenProcessToken)
+            // 6. Make a primary token with that token(DuplicateTokenEx)
+            // 7. Start the new process with that primary token(CreateProcessWithTokenW)
+
+            var hProcessToken = IntPtr.Zero;
+            // Enable SeIncreaseQuotaPrivilege in this process.  (This won't work if current process is not elevated.)
+            try
+            {
+                var process = GetCurrentProcess();
+                if (!OpenProcessToken(process, 0x0020, ref hProcessToken))
+                    return;
+
+                var tkp = new TOKEN_PRIVILEGES
+                {
+                    PrivilegeCount = 1,
+                    Privileges = new LUID_AND_ATTRIBUTES[1]
+                };
+
+                if (!LookupPrivilegeValue(null, "SeIncreaseQuotaPrivilege", ref tkp.Privileges[0].Luid))
+                    return;
+
+                tkp.Privileges[0].Attributes = 0x00000002;
+
+                if (!AdjustTokenPrivileges(hProcessToken, false, ref tkp, 0, IntPtr.Zero, IntPtr.Zero))
+                    return;
+            }
+            finally
+            {
+                CloseHandle(hProcessToken);
+            }
+
+            // Get an HWND representing the desktop shell.
+            // CAVEATS:  This will fail if the shell is not running (crashed or terminated), or the default shell has been
+            // replaced with a custom shell.  This also won't return what you probably want if Explorer has been terminated and
+            // restarted elevated.
+            var hwnd = GetShellWindow();
+            if (hwnd == IntPtr.Zero)
+                return;
+
+            var hShellProcess = IntPtr.Zero;
+            var hShellProcessToken = IntPtr.Zero;
+            var hPrimaryToken = IntPtr.Zero;
+            try
+            {
+                // Get the PID of the desktop shell process.
+                uint dwPID;
+                if (GetWindowThreadProcessId(hwnd, out dwPID) == 0)
+                    return;
+
+                // Open the desktop shell process in order to query it (get the token)
+                hShellProcess = OpenProcess(ProcessAccessFlags.QueryInformation, false, dwPID);
+                if (hShellProcess == IntPtr.Zero)
+                    return;
+
+                // Get the process token of the desktop shell.
+                if (!OpenProcessToken(hShellProcess, 0x0002, ref hShellProcessToken))
+                    return;
+
+                var dwTokenRights = 395U;
+
+                // Duplicate the shell's process token to get a primary token.
+                // Based on experimentation, this is the minimal set of rights required for CreateProcessWithTokenW (contrary to current documentation).
+                if (!DuplicateTokenEx(hShellProcessToken, dwTokenRights, IntPtr.Zero, SECURITY_IMPERSONATION_LEVEL.SecurityImpersonation, TOKEN_TYPE.TokenPrimary, out hPrimaryToken))
+                    return;
+
+                // Start the target process with the new token.
+                var si = new STARTUPINFO();
+                var pi = new PROCESS_INFORMATION();
+                if (!CreateProcessWithTokenW(hPrimaryToken, 0, fileName, "", 0, IntPtr.Zero, Path.GetDirectoryName(fileName), ref si, out pi))
+                    return;
+            }
+            finally
+            {
+                CloseHandle(hShellProcessToken);
+                CloseHandle(hPrimaryToken);
+                CloseHandle(hShellProcess);
+            }
+
+        }
         private static void UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
             var exception = (Exception)e.ExceptionObject;
