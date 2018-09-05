ea198835dda91ffd768958768841250a1c194835
ArachisH <iKBenPeanut@gmail.com>
2018-09-04 22:23:31 -0700

    I'm close, I can smell it

--- Tangine/Controls/TangineTabControl.cs
+++ Tangine/Controls/TangineTabControl.cs
@@ -1,8 +1,9 @@
 ﻿using System.Drawing;
 using System.Windows.Forms;
 using System.ComponentModel;
+using System;
 
-namespace Tangine.Controls
+namespace CTest
 {
     [DesignerCategory("Code")]
     public class TangineTabControl : TabControl
@@ -97,7 +98,7 @@ namespace Tangine.Controls
             var titleRegion = new Rectangle();
             bool isFirstTab = (tabIndex == 0);
 
-            titleRegion.X = (tabRegion.X + 2 + tabIndex);
+            titleRegion.X = (tabRegion.X + (isFirstTab ? 2 : (2 + tabIndex)));
             titleRegion.Y = (tabRegion.Y + 2);
 
             titleRegion.Width = tabRegion.Width;
@@ -130,53 +131,63 @@ namespace Tangine.Controls
             }
             if (TabPages.Count > 0)
             {
-                Rectangle tabRegion, titleRegion, glowRegion;
-                using (var skinBrush = new SolidBrush(Skin))
-                {
-                    var format = TextFormatFlags.VerticalCenter;
-                    for (int i = 0; i < TabPages.Count; i++)
-                    {
-                        tabRegion = GetTabRect(i);
-                        bool isSelected = (SelectedIndex == i);
-
-                        switch (Alignment)
-                        {
-                            default:
-                            case TabAlignment.Top:
-                            {
-                                format |= TextFormatFlags.HorizontalCenter;
-                                titleRegion = GetVerticalTitleRegion(i, -4);
-                                glowRegion = GetVerticalGlowRegion(tabRegion, titleRegion, tabRegion.Height - 2);
-                                break;
-                            }
-                            case TabAlignment.Bottom:
-                            {
-                                format |= TextFormatFlags.HorizontalCenter;
-                                titleRegion = GetVerticalTitleRegion(i, 0);
-                                glowRegion = GetVerticalGlowRegion(tabRegion, titleRegion, 0);
-                                break;
-                            }
-                            case TabAlignment.Left:
-                            {
-                                format |= TextFormatFlags.Right;
-                                titleRegion = GetHorizontalTitleRegion(i, -2);
-                                glowRegion = GetHorizontalGlowRegion(tabRegion, titleRegion, (titleRegion.X + tabRegion.Width));
-                                break;
-                            }
-                            case TabAlignment.Right:
-                            {
-                                format |= TextFormatFlags.Left;
-                                titleRegion = GetHorizontalTitleRegion(i, 4);
-                                glowRegion = GetHorizontalGlowRegion(tabRegion, titleRegion, tabRegion.X);
-                                break;
-                            }
-                        }
-                        e.Graphics.FillRectangle((isSelected ? skinBrush : Brushes.Silver), glowRegion);
-                        TextRenderer.DrawText(e.Graphics, TabPages[i].Text, Font, titleRegion, TitleColor, format);
-                    }
-                }
+                PaintTabItems(e);
             }
             base.OnPaint(e);
+        }
+        protected virtual void PaintTabItems(PaintEventArgs e)
+        {
+            Rectangle tabRegion, titleRegion, glowRegion;
+            using (var skinPen = new Pen(Skin))
+            {
+                var format = TextFormatFlags.VerticalCenter;
+                for (int i = 0; i < TabPages.Count; i++)
+                {
+                    bool isSelected = (SelectedIndex == i);
+                    tabRegion = GetTabRect(i);
+                    tabRegion.Offset(2 + ((2 * i) - i), 0);
+
+                    switch (Alignment)
+                    {
+                        default:
+                        case TabAlignment.Top:
+                        {
+                            format |= TextFormatFlags.HorizontalCenter;
+                            titleRegion = GetVerticalTitleRegion(i, -4);
+                            glowRegion = GetVerticalGlowRegion(tabRegion, titleRegion, tabRegion.Height - 2);
+                            break;
+                        }
+                        case TabAlignment.Bottom:
+                        {
+                            format |= TextFormatFlags.HorizontalCenter;
+                            titleRegion = GetVerticalTitleRegion(i, 0);
+                            glowRegion = GetVerticalGlowRegion(tabRegion, titleRegion, 0);
+                            break;
+                        }
+                        case TabAlignment.Left:
+                        {
+                            format |= TextFormatFlags.Right;
+                            titleRegion = GetHorizontalTitleRegion(i, -2);
+                            glowRegion = GetHorizontalGlowRegion(tabRegion, titleRegion, (titleRegion.X + tabRegion.Width));
+                            break;
+                        }
+                        case TabAlignment.Right:
+                        {
+                            format |= TextFormatFlags.Left;
+                            titleRegion = GetHorizontalTitleRegion(i, 4);
+                            glowRegion = GetHorizontalGlowRegion(tabRegion, titleRegion, tabRegion.X);
+                            break;
+                        }
+                    }
+
+                    int offset = 1;
+                    int xStart = tabRegion.X + (i == 0 ? 0 : offset);
+                    int xEnd = (tabRegion.X + tabRegion.Width) - (i == 0 ? offset * 2 : offset);
+
+                    e.Graphics.DrawLine((isSelected ? skinPen : Pens.Silver), xStart, tabRegion.Height, xEnd, tabRegion.Height);
+                    TextRenderer.DrawText(e.Graphics, /*TabPages[i].Text*/Math.Abs(xStart - xEnd).ToString(), Font, tabRegion, TitleColor, format);
+                }
+            }
         }
     }
 }
