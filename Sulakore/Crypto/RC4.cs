fb8477dbd3eaa5dc7af9269d5bd1a8cd7e9b8138
ArachisH <iKBenPeanut@gmail.com>
2018-09-20 11:04:58 -0700

    Thanks for the fix geeker

--- Sulakore/Crypto/RC4.cs
+++ Sulakore/Crypto/RC4.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Sulakore.Crypto
+﻿namespace Sulakore.Crypto
 {
     public class RC4
     {
@@ -51,52 +49,7 @@ namespace Sulakore.Crypto
                 return parsed;
             }
         }
-        public void RefParse(byte[] data)
-        {
-            RefParse(data, 0, data.Length);
-        }
-        public void RefParse(byte[] data, int length)
-        {
-            RefParse(data, 0, length);
-        }
-        public void RefParse(byte[] data, int offset, int length)
-        {
-            RefParse(data, offset, length, false);
-        }
-        public void RefParse(byte[] data, int offset, int length, bool isPeeking)
-        {
-            lock (_parseLock)
-            {
-                int i = _i;
-                int j = _j;
-                int[] pool = null;
-                if (isPeeking)
-                {
-                    pool = new int[_table.Length];
-                    Array.Copy(_table, pool, pool.Length);
-                }
-                for (int k = offset; k < length; k++)
-                {
-                    _i++;
-                    _i %= _table.Length;
-                    _j += _table[_i];
-                    _j %= _table.Length;
-                    Swap(_i, _j);
-
-                    int rightXOR = (_table[_i] + _table[_j]);
-                    rightXOR = _table[rightXOR % _table.Length];
-
-                    data[k] ^= (byte)rightXOR;
-                }
-                if (isPeeking)
-                {
-                    _i = i;
-                    _j = j;
-                    Array.Copy(pool, _table, _table.Length);
-                }
-            }
-        }
-
+        
         private void Swap(int a, int b)
         {
             int temp = _table[a];
