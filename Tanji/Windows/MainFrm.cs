baeedbb87b296ab577a54bbfa1fc1497775fa6c6
ArachisH <iKBenPeanut@gmail.com>
2019-07-31 19:19:06 -0700

    * Currently, all pre-shuffle connections will not send any connection details to Tanji

--- Tanji/Windows/MainFrm.cs
+++ Tanji/Windows/MainFrm.cs
@@ -55,8 +55,13 @@ namespace Tanji.Windows
         }
         public void Restore(ConnectedEventArgs e)
         {
-            HPacket endPointPkt = Master.Connection.Local.ReceivePacketAsync().Result;
-            e.HotelServer = ConnectionPg.HotelServer = HotelEndPoint.Parse(endPointPkt.ReadUTF8().Split('\0')[0], endPointPkt.ReadInt32());
+            // TODO: Implement EndPointShouter for pre-shuffle hotels.
+            if (Master.Game.IsPostShuffle)
+            {
+                HPacket endPointPkt = Master.Connection.Local.ReceivePacketAsync().Result;
+                e.HotelServer = ConnectionPg.HotelServer = HotelEndPoint.Parse(endPointPkt.ReadUTF8().Split('\0')[0], endPointPkt.ReadInt32());
+            }
+
             e.IsFakingPolicyRequest = e.HotelServer.Hotel == HHotel.Unknown;
             e.HotelServerSource.SetResult(e.HotelServer);
 
