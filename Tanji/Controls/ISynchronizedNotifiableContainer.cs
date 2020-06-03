43c1f66d7bdc747f1ae81b2e098b810128c14285
ArachisH <iKBenPeanut@gmail.com>
2020-06-03 16:53:14 -0700

    * Not sure yet, this seems like the correct path though

---
+++ Tanji/Controls/ISynchronizedNotifiableContainer.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Forms;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+
+namespace Tanji.Controls
+{
+    public interface ISynchronizedNotifiableContainer : INotifyPropertyChanged
+    {
+        bool IsInDesignMode { get; }
+        ContainerControl Container { get; }
+        Dictionary<string, Binding> Bindings { get; }
+
+        PropertyChangedEventHandler GetPropertyChangedEventHandler();
+
+        event PropertyChangedEventHandler asd;
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+           // findfo
+            //PropertyChangedEventHandler handler = PropertyChanged;
+            Container.FindForm().Invoke(GetPropertyChangedEventHandler(), this, e);
+            // PropertyChanged(this, e);
+            //PropertyChanged?.Invoke(this, e);
+        }
+        protected void RaiseOnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+
+    //public class TestClass : UserControl, ISynchronizedNotifiableContainer
+    //{
+    //    public bool IsInDesignMode => DesignMode;
+    //    public ContainerControl Container { get; }
+    //    public Dictionary<string, Binding> Bindings { get; }
+
+    //    public PropertyChangedEventHandler GetPropertyChangedEventHandler() => PropertyChanged;
+
+    //    public event PropertyChangedEventHandler asd;
+    //    public event PropertyChangedEventHandler PropertyChanged;
+    //}
+}
