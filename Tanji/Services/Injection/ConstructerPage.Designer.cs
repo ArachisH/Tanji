75e7869c13e14890727d8e0f63a2179264f5c642
ArachisH <iKBenPeanut@gmail.com>
2020-06-11 12:42:20 -0700

    * Completed ConstructerPage

--- Tanji/Services/Injection/ConstructerPage.Designer.cs
+++ Tanji/Services/Injection/ConstructerPage.Designer.cs
@@ -32,7 +32,7 @@
             this.MoveUpBtn = new Tangine.Controls.TangineButton();
             this.RemoveBtn = new Tangine.Controls.TangineButton();
             this.ClearBtn = new Tangine.Controls.TangineButton();
-            this.TransferBelowBtn = new Tangine.Controls.TangineButton();
+            this.CopyBtn = new Tangine.Controls.TangineButton();
             this.WriteBooleanBtn = new Tangine.Controls.TangineButton();
             this.WriteStringBtn = new Tangine.Controls.TangineButton();
             this.WriteIntegerBtn = new Tangine.Controls.TangineButton();
@@ -40,7 +40,6 @@
             this.ValuesVw = new Tangine.Controls.TangineListView();
             this.TypeCol = new System.Windows.Forms.ColumnHeader();
             this.ValueCol = new System.Windows.Forms.ColumnHeader();
-            this.EncodedCol = new System.Windows.Forms.ColumnHeader();
             this.DismantledTxt = new Tangine.Controls.TangineLabelBox();
             this.AmountTxt = new Tangine.Controls.TangineLabelBox();
             this.IDTxt = new Tangine.Controls.TangineLabelBox();
@@ -49,20 +48,22 @@
             // MoveDownBtn
             // 
             this.MoveDownBtn.Enabled = false;
-            this.MoveDownBtn.Location = new System.Drawing.Point(322, 256);
+            this.MoveDownBtn.Location = new System.Drawing.Point(216, 256);
             this.MoveDownBtn.Name = "MoveDownBtn";
             this.MoveDownBtn.Size = new System.Drawing.Size(100, 20);
             this.MoveDownBtn.TabIndex = 17;
             this.MoveDownBtn.Text = "Move Down";
+            this.MoveDownBtn.Click += new System.EventHandler(this.MoveDownBtn_Click);
             // 
             // MoveUpBtn
             // 
             this.MoveUpBtn.Enabled = false;
-            this.MoveUpBtn.Location = new System.Drawing.Point(216, 256);
+            this.MoveUpBtn.Location = new System.Drawing.Point(322, 256);
             this.MoveUpBtn.Name = "MoveUpBtn";
             this.MoveUpBtn.Size = new System.Drawing.Size(100, 20);
             this.MoveUpBtn.TabIndex = 16;
             this.MoveUpBtn.Text = "Move Up";
+            this.MoveUpBtn.Click += new System.EventHandler(this.MoveUpBtn_Click);
             // 
             // RemoveBtn
             // 
@@ -72,6 +73,7 @@
             this.RemoveBtn.Size = new System.Drawing.Size(100, 20);
             this.RemoveBtn.TabIndex = 15;
             this.RemoveBtn.Text = "Remove";
+            this.RemoveBtn.Click += new System.EventHandler(this.RemoveBtn_Click);
             // 
             // ClearBtn
             // 
@@ -81,19 +83,21 @@
             this.ClearBtn.Size = new System.Drawing.Size(100, 20);
             this.ClearBtn.TabIndex = 13;
             this.ClearBtn.Text = "Clear";
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
             // 
-            // TransferBelowBtn
+            // CopyBtn
             // 
-            this.TransferBelowBtn.Location = new System.Drawing.Point(3, 256);
-            this.TransferBelowBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.TransferBelowBtn.Name = "TransferBelowBtn";
-            this.TransferBelowBtn.Size = new System.Drawing.Size(100, 20);
-            this.TransferBelowBtn.TabIndex = 12;
-            this.TransferBelowBtn.Text = "Transfer Below";
+            this.CopyBtn.Location = new System.Drawing.Point(3, 256);
+            this.CopyBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.CopyBtn.Name = "CopyBtn";
+            this.CopyBtn.Size = new System.Drawing.Size(100, 20);
+            this.CopyBtn.TabIndex = 12;
+            this.CopyBtn.Text = "Copy";
+            this.CopyBtn.Click += new System.EventHandler(this.CopyBtn_Click);
             // 
             // WriteBooleanBtn
             // 
-            this.WriteBooleanBtn.Location = new System.Drawing.Point(219, 30);
+            this.WriteBooleanBtn.Location = new System.Drawing.Point(219, 29);
             this.WriteBooleanBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.WriteBooleanBtn.Name = "WriteBooleanBtn";
             this.WriteBooleanBtn.Size = new System.Drawing.Size(100, 20);
@@ -103,7 +107,7 @@
             // 
             // WriteStringBtn
             // 
-            this.WriteStringBtn.Location = new System.Drawing.Point(111, 30);
+            this.WriteStringBtn.Location = new System.Drawing.Point(111, 29);
             this.WriteStringBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.WriteStringBtn.Name = "WriteStringBtn";
             this.WriteStringBtn.Size = new System.Drawing.Size(100, 20);
@@ -113,7 +117,7 @@
             // 
             // WriteIntegerBtn
             // 
-            this.WriteIntegerBtn.Location = new System.Drawing.Point(3, 30);
+            this.WriteIntegerBtn.Location = new System.Drawing.Point(3, 29);
             this.WriteIntegerBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.WriteIntegerBtn.Name = "WriteIntegerBtn";
             this.WriteIntegerBtn.Size = new System.Drawing.Size(100, 20);
@@ -123,11 +127,12 @@
             // 
             // ValueTxt
             // 
-            this.ValueTxt.Location = new System.Drawing.Point(96, 4);
+            this.ValueTxt.Location = new System.Drawing.Point(96, 3);
             this.ValueTxt.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.ValueTxt.Name = "ValueTxt";
             this.ValueTxt.Size = new System.Drawing.Size(326, 20);
             this.ValueTxt.TabIndex = 1;
+            this.ValueTxt.TabStop = false;
             this.ValueTxt.Text = "";
             this.ValueTxt.TextPaddingWidth = 0;
             this.ValueTxt.Title = "Value";
@@ -138,66 +143,69 @@
             this.ValuesVw.CheckBoxes = true;
             this.ValuesVw.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.TypeCol,
-            this.ValueCol,
-            this.EncodedCol});
+            this.ValueCol});
             this.ValuesVw.FullRowSelect = true;
             this.ValuesVw.GridLines = true;
             this.ValuesVw.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.ValuesVw.HideSelection = false;
-            this.ValuesVw.Location = new System.Drawing.Point(3, 56);
+            this.ValuesVw.Location = new System.Drawing.Point(3, 55);
             this.ValuesVw.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.ValuesVw.MultiSelect = false;
             this.ValuesVw.Name = "ValuesVw";
             this.ValuesVw.ShowItemToolTips = true;
-            this.ValuesVw.Size = new System.Drawing.Size(419, 168);
+            this.ValuesVw.Size = new System.Drawing.Size(419, 169);
             this.ValuesVw.TabIndex = 0;
+            this.ValuesVw.TabStop = false;
             this.ValuesVw.UseCompatibleStateImageBehavior = false;
             this.ValuesVw.View = System.Windows.Forms.View.Details;
             this.ValuesVw.ItemActivate += new System.EventHandler(this.ValuesVw_ItemActivate);
+            this.ValuesVw.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.ValuesVw_ItemSelectionChanged);
             // 
             // TypeCol
             // 
             this.TypeCol.Name = "TypeCol";
             this.TypeCol.Text = "Type";
-            this.TypeCol.Width = 86;
+            this.TypeCol.Width = 199;
             // 
             // ValueCol
             // 
             this.ValueCol.Name = "ValueCol";
             this.ValueCol.Text = "Value";
-            this.ValueCol.Width = 165;
-            // 
-            // EncodedCol
-            // 
-            this.EncodedCol.Name = "EncodedCol";
-            this.EncodedCol.Text = "Encoded";
-            this.EncodedCol.Width = 164;
+            this.ValueCol.Width = 199;
             // 
             // DismantledTxt
             // 
+            this.DismantledTxt.IsReadOnly = true;
             this.DismantledTxt.Location = new System.Drawing.Point(3, 230);
             this.DismantledTxt.Name = "DismantledTxt";
             this.DismantledTxt.Size = new System.Drawing.Size(313, 20);
             this.DismantledTxt.TabIndex = 18;
+            this.DismantledTxt.TabStop = false;
             this.DismantledTxt.Text = "";
             this.DismantledTxt.Title = "Dismantled";
             // 
             // AmountTxt
             // 
-            this.AmountTxt.Location = new System.Drawing.Point(326, 30);
+            this.AmountTxt.IsNumbersOnly = true;
+            this.AmountTxt.Location = new System.Drawing.Point(326, 29);
+            this.AmountTxt.MaxLength = 2;
             this.AmountTxt.Name = "AmountTxt";
             this.AmountTxt.Size = new System.Drawing.Size(96, 20);
             this.AmountTxt.TabIndex = 19;
+            this.AmountTxt.TabStop = false;
             this.AmountTxt.Text = "";
             this.AmountTxt.Title = "Amount";
             // 
             // IDTxt
             // 
-            this.IDTxt.Location = new System.Drawing.Point(3, 4);
+            this.IDTxt.IsNumbersOnly = true;
+            this.IDTxt.Location = new System.Drawing.Point(3, 3);
+            this.IDTxt.MaxLength = 4;
             this.IDTxt.Name = "IDTxt";
             this.IDTxt.Size = new System.Drawing.Size(86, 20);
             this.IDTxt.TabIndex = 19;
-            this.IDTxt.Text = "";
+            this.IDTxt.TabStop = false;
+            this.IDTxt.Text = "1";
             this.IDTxt.Title = "ID";
             // 
             // ConstructerPage
@@ -211,7 +219,7 @@
             this.Controls.Add(this.MoveUpBtn);
             this.Controls.Add(this.RemoveBtn);
             this.Controls.Add(this.ClearBtn);
-            this.Controls.Add(this.TransferBelowBtn);
+            this.Controls.Add(this.CopyBtn);
             this.Controls.Add(this.WriteIntegerBtn);
             this.Controls.Add(this.WriteStringBtn);
             this.Controls.Add(this.WriteBooleanBtn);
@@ -229,11 +237,10 @@
         private System.Windows.Forms.ColumnHeader TypeCol;
         private System.Windows.Forms.ColumnHeader ValueCol;
         private Tangine.Controls.TangineLabelBox ValueTxt;
-        private System.Windows.Forms.ColumnHeader EncodedCol;
         private Tangine.Controls.TangineButton WriteIntegerBtn;
         private Tangine.Controls.TangineButton WriteStringBtn;
         private Tangine.Controls.TangineButton WriteBooleanBtn;
-        private Tangine.Controls.TangineButton TransferBelowBtn;
+        private Tangine.Controls.TangineButton CopyBtn;
         private Tangine.Controls.TangineButton ClearBtn;
         private Tangine.Controls.TangineButton MoveUpBtn;
         private Tangine.Controls.TangineButton MoveDownBtn;
--- Tanji/Services/Injection/ConstructerPage.cs
+++ Tanji/Services/Injection/ConstructerPage.cs
@@ -3,6 +3,7 @@ using System.Windows.Forms;
 using System.ComponentModel;
 
 using Tanji.Controls;
+using System.DirectoryServices.ActiveDirectory;
 
 namespace Tanji.Services.Injection
 {
@@ -10,16 +11,15 @@ namespace Tanji.Services.Injection
     [DesignerCategory("UserControl")]
     public partial class ConstructerPage : NotifiablePage
     {
-        private ushort _identifier = 0;
-        public ushort Identifier
+        private ushort _id = 1;
+        public ushort Id
         {
-            get => _identifier;
+            get => _id;
             set
             {
-                _identifier = value;
+                _id = value;
                 RaiseOnPropertyChanged();
-
-                Refresh();
+                Dismantle(true);
             }
         }
 
@@ -31,20 +31,17 @@ namespace Tanji.Services.Injection
             {
                 _amount = value;
                 RaiseOnPropertyChanged();
-
-                Refresh();
             }
         }
 
-        private string _structure = "{id:0}";
-        public string Structure
+        private string _value = string.Empty;
+        public string Value
         {
-            get => _structure;
+            get => _value;
             set
             {
-                _structure = value;
+                _value = value;
                 RaiseOnPropertyChanged();
-
                 Refresh();
             }
         }
@@ -53,9 +50,51 @@ namespace Tanji.Services.Injection
         {
             InitializeComponent();
 
-            Bind(IDTxt, "Text", nameof(Identifier));
-            Bind(DismantledTxt, "Text", nameof(Structure));
+            ValueTxt.Box.AutoCompleteMode = AutoCompleteMode.Append;
+            ValueTxt.Box.AutoCompleteSource = AutoCompleteSource.CustomSource;
+
+            Bind(IDTxt, "Text", nameof(Id));
+            Bind(ValueTxt, "Text", nameof(Value));
             Bind(AmountTxt, "Text", nameof(Amount));
+        }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            ValuesVw.Items.Clear();
+            Dismantle();
+        }
+        private void WriteIntegerBtn_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(Value, out int value))
+            {
+                WriteObject("Integer", value);
+            }
+        }
+        private void WriteBooleanBtn_Click(object sender, EventArgs e)
+        {
+            string value = Value.ToLower();
+            WriteObject("Boolean", value == "true" || value == "1");
+        }
+        private void WriteStringBtn_Click(object sender, EventArgs e) => WriteObject("String", Value);
+
+        private void CopyBtn_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(DismantledTxt.Text);
+        }
+        private void RemoveBtn_Click(object sender, EventArgs e)
+        {
+            ValuesVw.SelectedItem.Remove();
+            Dismantle();
+        }
+        private void MoveUpBtn_Click(object sender, EventArgs e)
+        {
+            ValuesVw.MoveSelectedItemUp();
+            Dismantle();
+        }
+        private void MoveDownBtn_Click(object sender, EventArgs e)
+        {
+            ValuesVw.MoveSelectedItemDown();
+            Dismantle();
         }
 
         private void ValueTxt_KeyDown(object sender, KeyEventArgs e)
@@ -63,22 +102,55 @@ namespace Tanji.Services.Injection
             if (e.KeyCode != Keys.Enter) return;
             if (!ValuesVw.HasSelectedItem) return;
 
-            string sValue = ValueTxt.Text;
-            string type = ValuesVw.SelectedItem.SubItems[0].Text;
-            
-            Refresh();
+            ValuesVw.SelectedItem.SubItems[1].Text = Value;
+            Dismantle();
         }
         private void ValuesVw_ItemActivate(object sender, EventArgs e)
         {
-            ValueTxt.Text = ValuesVw.SelectedItem.SubItems[1].Text;
+            Value = ValuesVw.SelectedItem.SubItems[1].Text;
             ValueTxt.Focus();
         }
+        private void ValuesVw_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            RemoveBtn.Enabled = ValuesVw.HasSelectedItem;
+            MoveUpBtn.Enabled = ValuesVw.CanMoveSelectedItemUp;
+            MoveDownBtn.Enabled = ValuesVw.CanMoveSelectedItemDown;
+        }
 
-        private void WriteIntegerBtn_Click(object sender, EventArgs e)
-        { }
-        private void WriteStringBtn_Click(object sender, EventArgs e)
-        { }
-        private void WriteBooleanBtn_Click(object sender, EventArgs e)
-        { }
+        private void Dismantle(bool idOnly = false)
+        {
+            string dismantled = $"{{id:{Id}}}";
+            if (!idOnly)
+            {
+                foreach (ListViewItem item in ValuesVw.Items)
+                {
+                    dismantled += $"{{{char.ToLower(item.Text[0])}:{item.Tag}}}";
+                }
+            }
+            else if (!string.IsNullOrWhiteSpace(DismantledTxt.Text))
+            {
+                int endIdIndex = DismantledTxt.Text.IndexOf('}');
+                dismantled += DismantledTxt.Text.Substring(endIdIndex + 1);
+            }
+            DismantledTxt.Text = dismantled;
+        }
+        private void WriteObject(string typeName, object value)
+        {
+            if (Amount > 1)
+            {
+                var subItems = new string[2] { typeName, value.ToString() };
+                var items = new ListViewItem[Amount];
+                for (int i = 0; i < Amount; i++)
+                {
+                    items[i] = new ListViewItem(subItems) { Tag = value };
+                }
+                ValuesVw.Items.AddRange(items);
+                ValuesVw.EnsureVisible(ValuesVw.Items.Count - 1);
+            }
+            else ValuesVw.AddItem(typeName, value.ToString()).Tag = value;
+
+            Dismantle();
+            ValueTxt.Box.AutoCompleteCustomSource.Add(value.ToString());
+        }
     }
 }
