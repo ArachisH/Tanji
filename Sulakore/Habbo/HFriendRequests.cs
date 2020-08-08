b9645f043f4deebd0848c4dbc4b826d1f2b17e5d
PrunusH <54079903+PrunusH@users.noreply.github.com>
2020-08-09 00:18:29 +0200

    * Update friend parsers

---
+++ Sulakore/Habbo/HFriendRequests.cs
@@ -0,0 +1,22 @@
+﻿using Sulakore.Protocol;
+
+namespace Sulakore.Habbo
+{
+    public class HFriendRequests
+    {
+        public int Count01 { get; set; }
+        public int Count02 { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Motto { get; set; }
+
+        public HFriendRequests(HMessage packet)
+        {
+            Count01 = packet.ReadInteger();
+            Count02 = packet.ReadInteger();
+            Id = packet.ReadInteger();
+            Name = packet.ReadString();
+            Motto = packet.ReadString();
+        }
+    }
+}
