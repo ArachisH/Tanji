61898aa33394d648955e99fbb79778a4a26d3c12
PrunusH <54079903+PrunusH@users.noreply.github.com>
2021-01-24 18:14:46 +0100

    * UI -> Options -> Add About

--- Tanji/Services/Options/AboutPage.Designer.cs
+++ Tanji/Services/Options/AboutPage.Designer.cs
@@ -28,12 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Sellout2Btn = new Tangine.Controls.TangineButton();
+            this.SelloutBtn = new Tangine.Controls.TangineButton();
+            this.HarbleDiscordBtn = new Tangine.Controls.TangineButton();
+            this.DonateBtn = new Tangine.Controls.TangineButton();
+            this.SpeaqerBtn = new Tangine.Controls.TangineButton();
+            this.SNGButton = new Tangine.Controls.TangineButton();
+            this.DarkboxBtn = new Tangine.Controls.TangineButton();
+            this.DarkStarBtn = new Tangine.Controls.TangineButton();
+            this.ArachisBtn = new Tangine.Controls.TangineButton();
             this.SuspendLayout();
+            // 
+            // Sellout2Btn
+            // 
+            this.Sellout2Btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(63)))), ((int)(((byte)(63)))));
+            this.Sellout2Btn.Location = new System.Drawing.Point(59, 206);
+            this.Sellout2Btn.Name = "Sellout2Btn";
+            this.Sellout2Btn.Size = new System.Drawing.Size(286, 20);
+            this.Sellout2Btn.TabIndex = 1;
+            this.Sellout2Btn.Text = "HabboInterceptor | Niewiarowski\'s Packet Logger";
+            this.Sellout2Btn.Click += new System.EventHandler(this.Sellout2Btn_Click);
+            // 
+            // SelloutBtn
+            // 
+            this.SelloutBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(63)))), ((int)(((byte)(63)))));
+            this.SelloutBtn.Location = new System.Drawing.Point(59, 180);
+            this.SelloutBtn.Name = "SelloutBtn";
+            this.SelloutBtn.Size = new System.Drawing.Size(286, 20);
+            this.SelloutBtn.TabIndex = 2;
+            this.SelloutBtn.Text = "G-Earth | Cross-Platform Packet Logger by sirjonasxx";
+            this.SelloutBtn.Click += new System.EventHandler(this.SelloutBtn_Click);
+            // 
+            // HarbleDiscordBtn
+            // 
+            this.HarbleDiscordBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(137)))), ((int)(((byte)(218)))));
+            this.HarbleDiscordBtn.Location = new System.Drawing.Point(91, 154);
+            this.HarbleDiscordBtn.Name = "HarbleDiscordBtn";
+            this.HarbleDiscordBtn.Size = new System.Drawing.Size(223, 20);
+            this.HarbleDiscordBtn.Skin = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(137)))), ((int)(((byte)(218)))));
+            this.HarbleDiscordBtn.TabIndex = 3;
+            this.HarbleDiscordBtn.Text = "Join our Discord! - Elbrah";
+            this.HarbleDiscordBtn.Click += new System.EventHandler(this.HarbleDiscordBtn_Click);
+            // 
+            // DonateBtn
+            // 
+            this.DonateBtn.BackColor = System.Drawing.Color.Green;
+            this.DonateBtn.Location = new System.Drawing.Point(197, 76);
+            this.DonateBtn.Name = "DonateBtn";
+            this.DonateBtn.Size = new System.Drawing.Size(117, 20);
+            this.DonateBtn.Skin = System.Drawing.Color.Green;
+            this.DonateBtn.TabIndex = 4;
+            this.DonateBtn.Text = "Donate";
+            this.DonateBtn.Click += new System.EventHandler(this.DonateBtn_Click);
+            // 
+            // SpeaqerBtn
+            // 
+            this.SpeaqerBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(106)))), ((int)(((byte)(218)))));
+            this.SpeaqerBtn.Location = new System.Drawing.Point(91, 128);
+            this.SpeaqerBtn.Name = "SpeaqerBtn";
+            this.SpeaqerBtn.Size = new System.Drawing.Size(100, 20);
+            this.SpeaqerBtn.Skin = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(106)))), ((int)(((byte)(218)))));
+            this.SpeaqerBtn.TabIndex = 5;
+            this.SpeaqerBtn.Text = "@SpeaqerDev";
+            this.SpeaqerBtn.Click += new System.EventHandler(this.SpeaqerBtn_Click);
+            // 
+            // SNGButton
+            // 
+            this.SNGButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(63)))), ((int)(((byte)(63)))));
+            this.SNGButton.Location = new System.Drawing.Point(197, 128);
+            this.SNGButton.Name = "SNGButton";
+            this.SNGButton.Size = new System.Drawing.Size(117, 20);
+            this.SNGButton.TabIndex = 6;
+            this.SNGButton.Text = "SnGForum.info";
+            this.SNGButton.Click += new System.EventHandler(this.SNGButton_Click);
+            // 
+            // DarkboxBtn
+            // 
+            this.DarkboxBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(63)))), ((int)(((byte)(63)))));
+            this.DarkboxBtn.Location = new System.Drawing.Point(197, 102);
+            this.DarkboxBtn.Name = "DarkboxBtn";
+            this.DarkboxBtn.Size = new System.Drawing.Size(117, 20);
+            this.DarkboxBtn.TabIndex = 7;
+            this.DarkboxBtn.Text = "Darkbox.nl";
+            this.DarkboxBtn.Click += new System.EventHandler(this.DarkboxBtn_Click);
+            // 
+            // DarkStarBtn
+            // 
+            this.DarkStarBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(106)))), ((int)(((byte)(218)))));
+            this.DarkStarBtn.Location = new System.Drawing.Point(91, 102);
+            this.DarkStarBtn.Name = "DarkStarBtn";
+            this.DarkStarBtn.Size = new System.Drawing.Size(100, 20);
+            this.DarkStarBtn.Skin = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(106)))), ((int)(((byte)(218)))));
+            this.DarkStarBtn.TabIndex = 8;
+            this.DarkStarBtn.Text = "@DarkStar851";
+            this.DarkStarBtn.Click += new System.EventHandler(this.DarkStarBtn_Click);
+            // 
+            // ArachisBtn
+            // 
+            this.ArachisBtn.BackColor = System.Drawing.Color.Purple;
+            this.ArachisBtn.Location = new System.Drawing.Point(91, 76);
+            this.ArachisBtn.Name = "ArachisBtn";
+            this.ArachisBtn.Size = new System.Drawing.Size(100, 20);
+            this.ArachisBtn.Skin = System.Drawing.Color.Purple;
+            this.ArachisBtn.TabIndex = 9;
+            this.ArachisBtn.Text = "@ArachisH";
+            this.ArachisBtn.Click += new System.EventHandler(this.ArachisBtn_Click);
             // 
             // AboutPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Sellout2Btn);
+            this.Controls.Add(this.SelloutBtn);
+            this.Controls.Add(this.HarbleDiscordBtn);
+            this.Controls.Add(this.DonateBtn);
+            this.Controls.Add(this.SpeaqerBtn);
+            this.Controls.Add(this.SNGButton);
+            this.Controls.Add(this.DarkboxBtn);
+            this.Controls.Add(this.DarkStarBtn);
+            this.Controls.Add(this.ArachisBtn);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Margin = new System.Windows.Forms.Padding(0);
             this.Name = "AboutPage";
@@ -43,5 +156,14 @@
         }
 
         #endregion
+        private Tangine.Controls.TangineButton Sellout2Btn;
+        private Tangine.Controls.TangineButton SelloutBtn;
+        private Tangine.Controls.TangineButton HarbleDiscordBtn;
+        private Tangine.Controls.TangineButton DonateBtn;
+        private Tangine.Controls.TangineButton SpeaqerBtn;
+        private Tangine.Controls.TangineButton SNGButton;
+        private Tangine.Controls.TangineButton DarkboxBtn;
+        private Tangine.Controls.TangineButton DarkStarBtn;
+        private Tangine.Controls.TangineButton ArachisBtn;
     }
 }
--- Tanji/Services/Options/AboutPage.cs
+++ Tanji/Services/Options/AboutPage.cs
@@ -12,5 +12,50 @@ namespace Tanji.Services.Options
         {
             InitializeComponent();
         }
+
+        private void ArachisBtn_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void DonateBtn_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void DarkStarBtn_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void DarkboxBtn_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void SpeaqerBtn_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void SNGButton_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void HarbleDiscordBtn_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void SelloutBtn_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void Sellout2Btn_Click(object sender, System.EventArgs e)
+        {
+
+        }
     }
 }
