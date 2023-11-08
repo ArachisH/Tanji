ff8284f51d157a27cf8456d800445414d8d9bfe5
Adolfo Huitron <adolfo@huitron.dev>
2023-11-08 01:36:48 -0800

    refactor: Add json converter for DateTime
    Read UInt64 values into DateTime(UnixEpoch+Milliseconds), which is then translated to local time.

---
+++ Tanji.Core/Configuration/Json/EpochDateTimeConverter.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Tanji.Core.Configuration.Json;
+
+internal sealed class EpochDateTimeConverter : JsonConverter<DateTime>
+{
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => DateTime.UnixEpoch.AddMilliseconds(reader.GetUInt64()).ToLocalTime();
+}
