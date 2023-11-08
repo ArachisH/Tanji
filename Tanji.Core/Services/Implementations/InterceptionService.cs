e9a957ef414259accaea20a8de56ea618a40dae3
Adolfo Huitron <adolfo@huitron.dev>
2023-11-08 02:12:36 -0800

    refactor: Implement LaunchInterceptableClientAsync
    Prior to launching any game client, we need to attempt to load up a .json file that contains the barebones required game information that relates to a specifics client version(MD5).
    
    Any HConnection instances created will be stored in a dictionary, and have the ticket that was used to initialize the session as their key.

--- Tanji.Core/Services/Implementations/InterceptionService.cs
+++ Tanji.Core/Services/Implementations/InterceptionService.cs
@@ -1,23 +1,37 @@
-﻿using System.Threading.Channels;
+﻿using System.Diagnostics;
+using System.Threading.Channels;
+using System.Security.Cryptography;
+using System.Collections.ObjectModel;
 
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
+using Tanji.Core.Habbo;
+using Tanji.Core.Network;
+using Tanji.Core.Habbo.Canvas;
+using Tanji.Core.Configuration;
+
 using Eavesdrop;
 
-using Tanji.Core.Habbo;
-using Tanji.Core.Configuration;
+using Flazzy.Tools;
+
+using CommunityToolkit.HighPerformance;
+using CommunityToolkit.HighPerformance.Buffers;
+using Tanji.Core.Habbo.Canvas.Flash;
 
 namespace Tanji.Core.Services;
 
 public sealed class InterceptionService : IInterceptionService
 {
-    private static ReadOnlySpan<char> TicketVariableName => "\"ticket\":\"";
-
-    private readonly Channel<string> _gameTicketsChannel;
-
     private readonly TanjiOptions _options;
+    private readonly DirectoryInfo _cacheDirectory;
+    private readonly Channel<string> _gameTicketsChannel;
     private readonly ILogger<InterceptionService> _logger;
+
+    private readonly Dictionary<string, HConnection> _connections;
+    private IReadOnlyDictionary<string, HConnection> Connections { get; init; }
+
+    private static ReadOnlySpan<char> TicketVariableName => "\"ticket\":\"";
 
     public bool IsInterceptingWebTraffic => Eavesdropper.IsRunning;
     public bool IsInterceptingGameTraffic => false;
@@ -26,13 +40,15 @@ public sealed class InterceptionService : IInterceptionService
     {
         _logger = logger;
         _options = options.Value;
+        _cacheDirectory = Directory.CreateDirectory("Cache");
+        _connections = new Dictionary<string, HConnection>(8);
         _gameTicketsChannel = Channel.CreateUnbounded<string>(new UnboundedChannelOptions()
         {
             SingleReader = true,
             SingleWriter = true
         });
 
-        _logger.LogInformation("InterceptionService ctor");
+        Connections = new ReadOnlyDictionary<string, HConnection>(_connections);
 
         Eavesdropper.Targets.AddRange(_options.ProxyOverrides);
         Eavesdropper.RequestInterceptedAsync += WebRequestInterceptedAsync;
@@ -40,10 +56,96 @@ public sealed class InterceptionService : IInterceptionService
         Eavesdropper.Certifier = new Certifier("Tanji", "Tanji Root Certificate Authority");
     }
 
-    public ValueTask<string> InterceptGameTicketAsync()
+    public ValueTask<string> InterceptGameTicketAsync(CancellationToken cancellationToken = default)
     {
         TryStartWebTrafficInterception();
-        return _gameTicketsChannel.Reader.ReadAsync();
+        return _gameTicketsChannel.Reader.ReadAsync(cancellationToken);
+    }
+    public ValueTask<HConnection> LaunchInterceptableClientAsync(string ticket, HPlatform platform, string? clientPath = null, CancellationToken cancellationToken = default)
+    {
+        FileInfo? clientFileInfo = null;
+        if (!string.IsNullOrWhiteSpace(clientPath) && File.Exists(clientPath))
+        {
+            clientFileInfo = new FileInfo(clientPath);
+        }
+
+        PlatformPaths paths = default;
+        if (clientFileInfo == null)
+        {
+            if (_options.PlatformPaths == null)
+            {
+                throw new ArgumentNullException(nameof(clientPath), "No client/executable path available for fallback, provide a valid client file path.");
+            }
+
+            if (!_options.PlatformPaths.TryGetValue(platform, out paths))
+            {
+                throw new ArgumentException("No file paths available for the specified platform.", nameof(platform));
+            }
+
+            clientFileInfo = new FileInfo(paths.ClientPath);
+        }
+
+        if (!clientFileInfo.Exists)
+        {
+            ThrowHelper.ThrowFileNotFoundException("The provided client file path does not exist.", clientFileInfo.FullName);
+        }
+
+        Span<byte> hash = stackalloc byte[16];
+        using FileStream clientFs = clientFileInfo.OpenRead();
+
+        MD5.HashData(clientFs, hash);
+        hash = hash.Slice(0, 4);
+
+        CachedGame? cachedGame = null;
+        string uniqueName = Convert.ToHexString(hash);
+        foreach (var fileInfo in _cacheDirectory.EnumerateFileSystemInfos())
+        {
+            // {REVISION}_{32BITMD5HEAD}.json
+            if (!fileInfo.Name.EndsWith($"{uniqueName}.json")) continue;
+            cachedGame = new CachedGame(fileInfo.FullName);
+        }
+
+        if (cachedGame == null)
+        {
+            switch (platform)
+            {
+                case HPlatform.Flash:
+                {
+                    using var flashClientBuffer = MemoryOwner<byte>.Allocate((int)clientFileInfo.Length);
+                    Span<byte> flashClientSpan = flashClientBuffer.Span;
+
+                    using FileStream flashClientFs = clientFileInfo.OpenRead();
+                    flashClientFs.Read(flashClientSpan);
+
+                    // Attempt to decrypt the flash file.
+                    int decryptedLength = DecryptFlashClient(ref flashClientSpan, out int writtenOffset);
+
+                    // Either re-use the same initial buffer, or slice out the decrypted data.
+                    using MemoryOwner<byte> decryptedFlashClientBuffer = decryptedLength > 0 ?
+                        flashClientBuffer.Slice(writtenOffset, decryptedLength) : flashClientBuffer;
+
+                    // We can dispose these instances, since we're only using them to extract data into a more lightweight type.
+                    using var flashGame = new FlashGame(clientFileInfo.FullName, decryptedFlashClientBuffer.AsStream());
+
+                    cachedGame = CacheModifiedClient(flashGame, in paths);
+                    break;
+                }
+
+                default:
+                case HPlatform.Unity:
+                case HPlatform.HTML5:
+                case HPlatform.Unknown:
+                case HPlatform.Shockwave: break;
+            }
+        }
+
+        // TODO: Populate with relevant game client information.
+        var connection = new HConnection() { };
+
+        // TODO: Begin packet interception, then return the task.
+        Process.Start(paths.ExecutablePath, $"server {ticket[..4]} ticket {ticket[5..]}");
+
+        return ValueTask.FromResult(connection);
     }
 
     private Task WebRequestInterceptedAsync(object? sender, RequestInterceptedEventArgs e)
@@ -74,19 +176,23 @@ public sealed class InterceptionService : IInterceptionService
         }
     }
 
-
     private bool TryStartWebTrafficInterception()
     {
         if (Eavesdropper.IsRunning) return true;
-        if (!Eavesdropper.Certifier.CreateTrustedRootCertificate())
+        if (!Eavesdropper.Certifier?.CreateTrustedRootCertificate() ?? false)
         {
-            _logger.LogWarning("User declined root certificate installation");
+            _logger.LogWarning("User declined root certificate installation.");
             return false;
         }
 
         Eavesdropper.Initiate(_options.ProxyListenPort);
         return true;
     }
+    private CachedGame CacheModifiedClient(HGame game, in PlatformPaths paths)
+    {
+        throw new NotSupportedException();
+    }
+
     private static bool TryExtractTicket(ReadOnlySpan<char> body, out string? ticket)
     {
         ticket = null;
@@ -102,5 +208,12 @@ public sealed class InterceptionService : IInterceptionService
         }
 
         return false;
+    }
+    private static int DecryptFlashClient(ref Span<byte> flashClientBuffer, out int writtenOffset)
+    {
+        writtenOffset = 0;
+        return flashClientBuffer[0] <= 'Z'
+            ? flashClientBuffer.Length
+            : FlashCrypto.Decrypt(ref flashClientBuffer, out writtenOffset);
     }
 }
