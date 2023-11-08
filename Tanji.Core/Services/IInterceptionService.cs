49cd349e2b104cd68e9782bc4e5deae81a08decc
Adolfo Huitron <adolfo@huitron.dev>
2023-11-08 01:47:48 -0800

    refactor: Update interface requirements
    Add method for launching the game client of the specified platform, with the provided ticket, and optionally, the custom client path.
    
    CancellationToken values can now be passed as a method argument to ensure a graceful finish.

--- Tanji.Core/Services/IInterceptionService.cs
+++ Tanji.Core/Services/IInterceptionService.cs
@@ -1,9 +1,13 @@
-﻿namespace Tanji.Core.Services;
+﻿using Tanji.Core.Network;
+using Tanji.Core.Habbo.Canvas;
+
+namespace Tanji.Core.Services;
 
 public interface IInterceptionService
 {
     public bool IsInterceptingWebTraffic { get; }
     public bool IsInterceptingGameTraffic { get; }
 
-    public ValueTask<string> InterceptGameTicketAsync();
+    public ValueTask<string> InterceptGameTicketAsync(CancellationToken cancellationToken = default);
+    public Task<HConnection> LaunchInterceptableClientAsync(string ticket, HPlatform platform, string? clientPath = null, CancellationToken cancellationToken = default);
 }
