d8dc2b1a9d50f3abf19ca27cbd972cbefa6bcf38
Adolfo Huitron <adolfo@huitron.dev>
2023-11-11 00:26:55 -0800

    refactor: Add debugger display attribute
    Shows some useful information via the tooltip

--- Tanji.Core/Habbo/Canvas/Flash/FlashMessage.cs
+++ Tanji.Core/Habbo/Canvas/Flash/FlashMessage.cs
@@ -1,5 +1,25 @@
-﻿using Flazzy.ABC;
+﻿using System.Text;
+using System.Diagnostics;
+
+using Flazzy.ABC;
 
 namespace Tanji.Core.Habbo.Canvas.Flash;
 
-public readonly record struct FlashMessage(short Id, string? Structure, bool IsOutgoing, ASClass MessageClass, ASClass? ParserClass, List<FlashMessageReference> References);
+[DebuggerDisplay("{ToString()}")]
+public readonly record struct FlashMessage(short Id, string? Structure, bool IsOutgoing, ASClass MessageClass, ASClass? ParserClass, List<FlashMessageReference> References)
+{
+    public override string ToString()
+    {
+        var builder = new StringBuilder();
+        builder.Append($"({Id}, {MessageClass.QName.Name}");
+        if (ParserClass != null)
+        {
+            builder.Append($", {ParserClass.QName.Name}");
+        }
+        if (!string.IsNullOrWhiteSpace(Structure))
+        {
+            builder.Append($", {Structure}");
+        }
+        return builder.Append(')').ToString();
+    }
+}
