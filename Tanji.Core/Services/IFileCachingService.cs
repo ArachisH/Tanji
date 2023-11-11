1954a00cb3e788ddabdc2ff0900ffd70b493dfce
Adolfo Huitron <adolfo@huitron.dev>
2023-11-11 00:45:09 -0800

    refactor: Add interface for describing file caching service implementations

---
+++ Tanji.Core/Services/IFileCachingService.cs
@@ -0,0 +1,8 @@
+﻿namespace Tanji.Core.Services;
+
+public interface IFileCachingService<TContext, TCached>
+{
+    public DirectoryInfo Root { get; init; }
+
+    public TCached GetOrAdd(TContext context);
+}
