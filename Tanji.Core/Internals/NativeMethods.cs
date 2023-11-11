09164db808ff0da7b0211376700393ffac749c06
Adolfo Huitron <adolfo@huitron.dev>
2023-11-11 00:43:19 -0800

    refactor: Add native method import for generating hard links

---
+++ Tanji.Core/Internals/NativeMethods.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Tanji.Core.Internals;
+
+internal static partial class NativeMethods
+{
+    [LibraryImport("Kernel32.dll", EntryPoint = "CreateHardLinkA", StringMarshalling = StringMarshalling.Utf8)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool CreateHardLink(string lpFileName, string lpExistingFileName, IntPtr lpSecurityAttributes);
+}
