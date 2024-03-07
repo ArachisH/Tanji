8ef3ab72cbb62850b706fc8a6ad08f1f84e2deb8
Adolfo Huitron <adolfo@huitron.dev>
2024-03-06 21:12:13 -0800

    feat: add service properties

--- Tanji.Core/Services/IConnectionHandlerService.cs
+++ Tanji.Core/Services/IConnectionHandlerService.cs
@@ -1,6 +1,12 @@
-﻿namespace Tanji.Core.Services;
+﻿using System.Collections.ObjectModel;
+
+using Tanji.Core.Network;
+
+namespace Tanji.Core.Services;
 
 public interface IConnectionHandlerService
 {
+    ObservableCollection<HConnection> Connections { get; }
 
+    Task<HConnection> LaunchAndInterceptConnectionAsync(string ticket, HConnectionContext options, CancellationToken cancellationToken = default);
 }
