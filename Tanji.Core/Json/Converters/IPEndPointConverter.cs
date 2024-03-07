fcab39a275fb4a11ba05b98f2b7a03d223be35e7
Adolfo Huitron <adolfo@huitron.dev>
2024-03-06 18:48:41 -0800

    feat: add IPEndPoint json converter

---
+++ Tanji.Core/Json/Converters/IPEndPointConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Tanji.Core.Json.Converters;
+
+public sealed class IPEndPointConverter : JsonConverter<IPEndPoint>
+{
+    public override IPEndPoint? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        string? value = reader.GetString();
+        return !string.IsNullOrWhiteSpace(value) ? IPEndPoint.Parse(value) : null;
+    }
+
+    public override void Write(Utf8JsonWriter writer, IPEndPoint value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString());
+    }
+}
