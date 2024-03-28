09145e76be8b00b76df993d9542c7ababe24b25d
Adolfo Huitron <adolfo@huitron.dev>
2024-03-27 19:00:42 -0700

    chore: add service for providing HConnection instances through DI

---
+++ Tanji.Infrastructure/Factories/IConnectionFactory.cs
@@ -0,0 +1,9 @@
+﻿using Tanji.Core.Network;
+using Tanji.Core.Habbo.Network;
+
+namespace Tanji.Infrastructure.Factories;
+
+public interface IConnectionFactory
+{
+    IHConnection Create(HConnectionContext context);
+}
---
+++ Tanji.Infrastructure/Factories/Implementations/ConnectionFactory.cs
@@ -0,0 +1,24 @@
+﻿using Tanji.Core.Network;
+using Tanji.Core.Habbo.Canvas;
+using Tanji.Core.Habbo.Network;
+
+namespace Tanji.Infrastructure.Factories.Implementations;
+
+public sealed class ConnectionFactory : IConnectionFactory
+{
+    private readonly IServiceProvider _services;
+
+    public ConnectionFactory(IServiceProvider services)
+    {
+        _services = services;
+    }
+
+    public IHConnection Create(HConnectionContext context)
+    {
+        return context.Platform switch
+        {
+            HPlatform.Flash => new HConnection(context),
+            _ => throw new NotSupportedException($"{context.Platform} is currently not a supported platform.")
+        };
+    }
+}
