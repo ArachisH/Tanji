fd61d7c4957409857001eaac19c9da159f1c7c35
Adolfo Huitron <adolfo@huitron.dev>
2024-03-27 18:53:10 -0700

    chore: remove unsed type

--- Tanji.Infrastructure/Services/IPacketMiddlemanService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Tanji.Core.Habbo.Network;
-
-namespace Tanji.Infrastructure.Services;
-
-public interface IPacketMiddlemanService
-{
-    ValueTask<bool> PacketInboundAsync(Memory<byte> buffer, HNode destination);
-    ValueTask<bool> PacketOutboundAsync(Memory<byte> buffer, HNode destination);
-}
