d6e844dcfd757b88a025881a63f5f4bd264ba458
Adolfo Huitron <adolfo@huitron.dev>
2024-03-27 18:34:11 -0700

    refactor: place 'PlatformPaths' into its own file

---
+++ Tanji.Infrastructure/Configuration/PlatformPaths.cs
@@ -0,0 +1,12 @@
+﻿using Tanji.Core.Habbo.Canvas;
+
+namespace Tanji.Infrastructure.Configuration;
+
+public readonly record struct PlatformPaths
+{
+    public required HPlatform Platform { get; init; }
+
+    public readonly string RootPath { get; init; }
+    public required string ClientPath { get; init; }
+    public required string ExecutablePath { get; init; }
+}
--- Tanji.Infrastructure/Configuration/TanjiOptions.cs
+++ Tanji.Infrastructure/Configuration/TanjiOptions.cs
@@ -6,15 +6,6 @@ using Tanji.Core.Habbo.Canvas;
 
 namespace Tanji.Infrastructure.Configuration;
 
-public readonly record struct PlatformPaths
-{
-    public required HPlatform Platform { get; init; }
-
-    public readonly string RootPath { get; init; }
-    public required string ClientPath { get; init; }
-    public required string ExecutablePath { get; init; }
-}
-
 public sealed class TanjiOptions
 {
     public required string[] UnityInterceptionTriggers { get; init; }
