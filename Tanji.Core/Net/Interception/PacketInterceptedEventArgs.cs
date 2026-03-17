facc05ae2cf23ab3b62f130143870f4e879be3d5
Adolfo Huitron <adolfo@huitron.dev>
2026-03-17 01:13:11 -0700

    feat: add type to represent intercepted packets
    This type will be replacing DataInterceptedEventArgs.

---
+++ Tanji.Core/Net/Interception/PacketInterceptedEventArgs.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+
+using Tanji.Core.Net.Formats;
+
+namespace Tanji.Core.Net.Interception;
+
+public sealed class PacketInterceptedEventArgs : CancelEventArgs
+{
+    private Memory<byte> _packetBuffer;
+    private ReadOnlyMemory<byte>? _roPacketBuffer;
+
+    public bool IsOutbound { get; }
+    public IHFormat PacketFormat { get; }
+    public ReadOnlyMemory<byte> PacketBuffer => _roPacketBuffer ?? _packetBuffer;
+
+    public bool IsReplaced { get; private set; }
+    /// <summary>
+    /// Determines whether the underlying packet buffer is read-only.
+    /// </summary>
+    public bool IsReadOnly { get; private set; }
+
+    public PacketInterceptedEventArgs(Memory<byte> originalPacketBuffer, IHFormat packetFormat, bool isOutbound)
+    {
+        _packetBuffer = originalPacketBuffer;
+
+        IsOutbound = isOutbound;
+        PacketFormat = packetFormat;
+    }
+
+    public ref Memory<byte> GetMutablePacketBuffer()
+    {
+        if (IsReadOnly)
+        {
+            throw new Exception("Packet buffer is read-only.");
+        }
+
+        // Aassume data has been changed.
+        IsReplaced = true;
+        return ref _packetBuffer;
+    }
+
+    /// <summary>
+    /// Replaces packet with a mutable packet buffer.
+    /// Encryption process will alter the buffer.
+    /// </summary>
+    /// <param name="packetBuffer"></param>
+    public void Replace(Memory<byte> packetBuffer)
+    {
+        _packetBuffer = packetBuffer;
+        IsReplaced = true;
+    }
+    /// <summary>
+    /// Replaces packet with a non-mutable buffer.
+    /// Copying will occur during the encryption process.
+    /// </summary>
+    /// <param name="packetBuffer"></param>
+    public void Replace(ReadOnlyMemory<byte> packetBuffer)
+    {
+        _roPacketBuffer = packetBuffer;
+        IsReadOnly = true;
+        IsReplaced = true;
+    }
+}
