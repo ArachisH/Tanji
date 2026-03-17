19cf9faccbba45eed8f924e7c9c8943dc661e536
Adolfo Huitron <adolfo@huitron.dev>
2026-03-17 03:48:19 -0700

    refactor: mimic namespace structure to the Tanji.Core project library

--- Tanji/Core/Infrastructure/Services/ServiceCollectionExtensions.cs
+++ Tanji/Core/Infrastructure/Services/ServiceCollectionExtensions.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 using WindowsFormsLifetime;
 
-namespace Tanji.Utilities;
+namespace Tanji.Core.Infrastructure.Services;
 
 public static class ServiceCollectionExtensions
 {
--- Tanji/Program.cs
+++ Tanji/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 
 using Tanji.Views;
-using Tanji.Utilities;
 using Tanji.Core.Infrastructure.Services;
 using Tanji.Core.Infrastructure.Configuration;
 
