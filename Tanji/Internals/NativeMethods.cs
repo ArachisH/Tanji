67e54e1e23a05e378909462952828a7555af0171
Adolfo Huitron <adolfo@huitron.dev>
2026-03-25 02:21:02 -0700

    build: add P/Invoke native methods
    Add NativeMethods with LibraryImport bindings for ShowCaret, HideCaret, and SendMessage.
    
    Disable runtime marshalling via AssemblyInfo to support source-generated P/Invoke.
    
    Enable AllowUnsafeBlocks in project file.

---
+++ Tanji/Internals/NativeMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Tanji.Internals;
+
+internal static partial class NativeMethods
+{
+    [LibraryImport("user32.dll", EntryPoint = "ShowCaret")]
+    public static partial long ShowCaret(IntPtr hwnd);
+
+    [LibraryImport("user32.dll", EntryPoint = "HideCaret")]
+    public static partial long HideCaret(IntPtr hwnd);
+
+    [LibraryImport("user32.dll")]
+    public static partial IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, IntPtr lParam);
+
+    [LibraryImport("user32.dll")]
+    public static partial IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, ref Point lParam);
+}
