653a9c6adb827e1d7e13b87e97ec265412cc580f
Adolfo Huitron <adolfo@huitron.dev>
2026-03-25 02:25:57 -0700

    feat: add TanjiPacketViewer to PacketLoggerView

--- Tanji/Views/PacketLoggerView.Designer.cs
+++ Tanji/Views/PacketLoggerView.Designer.cs
@@ -30,15 +30,35 @@ partial class PacketLoggerView
     {
         components = new System.ComponentModel.Container();
         viewModelSrc = new System.Windows.Forms.BindingSource(components);
+        loggerVw = new Tanji.Controls.TanjiPacketViewer();
         ((System.ComponentModel.ISupportInitialize)viewModelSrc).BeginInit();
         SuspendLayout();
+        // 
+        // loggerVw
+        // 
+        loggerVw.BackColor = System.Drawing.Color.FromArgb(22, 22, 22);
+        loggerVw.BorderStyle = System.Windows.Forms.BorderStyle.None;
+        loggerVw.Dock = System.Windows.Forms.DockStyle.Fill;
+        loggerVw.Font = new System.Drawing.Font("Consolas", 11F);
+        loggerVw.ForeColor = System.Drawing.Color.White;
+        loggerVw.HideSelection = false;
+        loggerVw.IsHidingCaret = false;
+        loggerVw.Location = new System.Drawing.Point(0, 0);
+        loggerVw.Name = "loggerVw";
+        loggerVw.ReadOnly = true;
+        loggerVw.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+        loggerVw.ShowSelectionMargin = true;
+        loggerVw.Size = new System.Drawing.Size(1281, 849);
+        loggerVw.TabIndex = 1;
+        loggerVw.Text = "";
         // 
         // PacketLoggerView
         // 
         AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         BackColor = System.Drawing.Color.White;
-        ClientSize = new System.Drawing.Size(735, 551);
+        ClientSize = new System.Drawing.Size(1281, 849);
+        Controls.Add(loggerVw);
         Name = "PacketLoggerView";
         StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
         Text = "Tanji - Packet Logger";
@@ -49,4 +69,5 @@ partial class PacketLoggerView
     #endregion
 
     private System.Windows.Forms.BindingSource viewModelSrc;
+    private Controls.TanjiPacketViewer loggerVw;
 }
