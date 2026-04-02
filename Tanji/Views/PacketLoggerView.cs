0c447423bba21f9b1ed4ebed9b3e00437a801185
Adolfo Huitron <adolfo@huitron.dev>
2026-04-02 07:04:24 -0700

    feat: implement the log for rendering intercepted packets
    Consume PacketLog objects from the injected IPacketLogHandlerService type.
    
    Handle duplicate/repeated packets by instead appending an 'xN' marker by the separator to reduce drawing events for repeated packet logs.

--- Tanji/Views/PacketLoggerView.cs
+++ Tanji/Views/PacketLoggerView.cs
@@ -1,21 +1,84 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
+using System.Threading.Tasks;
 
-using Tanji.Properties;
-using Tanji.Core.Infrastructure.ViewModels;
-
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 
 using CommunityToolkit.Mvvm.ComponentModel;
+
+using Tanji.Properties;
+using Tanji.Core.Infrastructure.Models;
+using Tanji.Core.Infrastructure.Services;
+using Tanji.Core.Infrastructure.ViewModels;
 
 namespace Tanji.Views;
 
 public partial class PacketLoggerView : Form
 {
-    public PacketLoggerView()
+    private readonly ILogger<PacketLoggerView> _logger;
+    private IPacketLogHandlerService _packetLogHandler;
+    private readonly PacketLoggerViewModel _packetLoggerViewModel;
+
+    public PacketLoggerView(ILogger<PacketLoggerView> logger,
+        PacketLoggerViewModel packetLoggerViewModel,
+        IPacketLogHandlerService packetLogHandler)
     {
+        _logger = logger;
+        _packetLogHandler = packetLogHandler;
+        _packetLoggerViewModel = packetLoggerViewModel;
+
         InitializeComponent();
         Icon = Resources.Tanji_256;
+
+        _ = RenderPacketLogsAsync();
+    }
+
+    private async Task RenderPacketLogsAsync()
+    {
+        while (await _packetLogHandler.WaitForPacketLogsAsync())
+        {
+            PacketLog pLog = await _packetLogHandler.ReadPacketLogAsync();
+
+            // Suspend Text Drawing
+            loggerVw.SuspendPaint();
+
+            if (pLog.Repetitions < 2)
+            {
+                int start = loggerVw.TextLength;
+                loggerVw.AppendText(pLog.ToString());
+                foreach ((int length, Color highlight) in pLog.GetHighlights())
+                {
+                    HighlightText(start, length, highlight);
+                    start += length;
+                }
+            }
+            else
+            {
+                int previousLineIndex = loggerVw.Lines.Length - 2;
+                string previousLine = loggerVw.Lines[previousLineIndex];
+                int previousLineFirstCharIndex = loggerVw.Find(previousLine, RichTextBoxFinds.Reverse | RichTextBoxFinds.NoHighlight);
+
+                HighlightText(previousLineFirstCharIndex, previousLine.Length, _packetLoggerViewModel.PacketLoggingOptions.RepeatedHighlight);
+                loggerVw.SelectedText = $"--------------- x{pLog.Repetitions}";
+            }
+
+            // Resume Text Drawing
+            loggerVw.ResumePaint();
+
+            if (_packetLoggerViewModel.PacketLoggingOptions.IsAutoScrolling)
+            {
+                loggerVw.ScrollToBottom();
+            }
+        }
+    }
+
+    private void HighlightText(int start, int length, Color highlight)
+    {
+        loggerVw.SelectionStart = start;
+        loggerVw.SelectionLength = length;
+        loggerVw.SelectionColor = highlight;
     }
 
     protected override void OnLoad(EventArgs e)
