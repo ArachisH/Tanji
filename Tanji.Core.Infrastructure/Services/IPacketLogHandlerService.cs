45291df2992bc42c1acfcf69b1d4279e97dcc1e7
Adolfo Huitron <adolfo@huitron.dev>
2026-04-02 04:33:11 -0700

    chore: add IPacketLogHandlerService interface
    Types that inherit this interface must build/queue PacketLog objects for displaying in Views.

---
+++ Tanji.Core.Infrastructure/Services/IPacketLogHandlerService.cs
@@ -0,0 +1,14 @@
+﻿using Tanji.Core.Net.Formats;
+using Tanji.Core.Net.Messages;
+using Tanji.Core.Infrastructure.Models;
+
+namespace Tanji.Core.Infrastructure.Services;
+
+public interface IPacketLogHandlerService
+{
+    ValueTask<bool> WaitForPacketLogsAsync(CancellationToken cancellationToken = default);
+    ValueTask<PacketLog> ReadPacketLogAsync(CancellationToken cancellationToken = default);
+
+    PacketLog? WritePacketLog(ReadOnlySpan<byte> packetBufferSpan, in HMessage message);
+    PacketLog? WritePacketLog(ReadOnlySpan<byte> packetBufferSpan, bool isOutgoing, IHFormat format, string? revision);
+}
