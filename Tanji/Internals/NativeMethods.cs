7740fd7adf659cd4e54d50545808af615724fe72
Adolfo Huitron <adolfo@huitron.dev>
2026-04-02 04:24:54 -0700

    fix: revert to using DllImport for SendMessage P\Invoke call
    The rendering of the control was unable to be resumed after suspending drawing events.

--- Tanji/Internals/NativeMethods.cs
+++ Tanji/Internals/NativeMethods.cs
@@ -1,20 +1,16 @@
 ﻿using System;
-using System.Drawing;
 using System.Runtime.InteropServices;
 
 namespace Tanji.Internals;
 
 internal static partial class NativeMethods
 {
-    [LibraryImport("user32.dll", EntryPoint = "ShowCaret")]
+    [LibraryImport("user32.dll", EntryPoint = "ShowCaret", SetLastError = true)]
     public static partial long ShowCaret(IntPtr hwnd);
 
-    [LibraryImport("user32.dll", EntryPoint = "HideCaret")]
+    [LibraryImport("user32.dll", EntryPoint = "HideCaret", SetLastError = true)]
     public static partial long HideCaret(IntPtr hwnd);
 
-    [LibraryImport("user32.dll")]
-    public static partial IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, IntPtr lParam);
-
-    [LibraryImport("user32.dll")]
-    public static partial IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, ref Point lParam);
+    [DllImport("user32.dll", EntryPoint = "SendMessage", SetLastError = true)]
+    public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, IntPtr lParam);
 }
